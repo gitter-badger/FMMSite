96e8361e4a769cbf2d9756320adfb267c77066d9
Simonwtaylor <Simonwtaylor93@gmail.com>
2015-05-15 08:09:30 +0100

    cleaning
    title

--- app.js
+++ app.js
@@ -1,117 +1,119 @@
 /* ****Code for the Summoner look up function using JSON****  */
 /* Begin Inital function declaration  */
-function summonerLookUp(){
-    
-/* Initialises and sets the value of SUMMONER_NAME to equal a blank string  */    
-var SUMMONER_NAME="";
-    
-/*Sets the summoner name = to what the API throws back      */
-SUMMONER_NAME = $("#summoner").val();
-    
-/*If the summoner name is valid and thus retrieved, then this function executes */
-if(SUMMONER_NAME !== ""){
-    
-/* Use the dollar sign dot notation with ajax to start running an ajax script */    
-$.ajax({
-    
-/* This is the API request algorithm - asks the api for a particular function and returns the information */    
-url:
-'https://euw.api.pvp.net/api/lol/euw/v1.4/summoner/by-name/'+ SUMMONER_NAME + '?api_key=c53b69b4-e95f-491a-a79c-e8d8b34ab98f',
-type: 'GET', /* Tells the script that this aims to 'retrieve' information  */
-dataType:'json', /* Specify the data returned is JSON   */
-data: {
+$document.ready(function(){
+    function summonerLookUp(){
 
-},
-    
-/* If the script running is succcessful then this code block will execute */    
-success:
-function(json){
+    /* Initialises and sets the value of SUMMONER_NAME to equal a blank string  */    
+    var SUMMONER_NAME="";
 
-/* When the summoner name is entered without spaces -> inserts spaces into the string   */    
-var SUMMONER_NAME_NOSPACES = SUMMONER_NAME.replace("","");
-SUMMONER_NAME_NOSPACES = SUMMONER_NAME_NOSPACES.toLowerCase().trim();
+    /*Sets the summoner name = to what the API throws back      */
+    SUMMONER_NAME = $("#summoner").val();
 
-/* Initialises All values to be used in the search function - var = json[summname].valinapi    */    
-summonerLevel=json[SUMMONER_NAME_NOSPACES].summonerLevel;
-summonerID=json[SUMMONER_NAME_NOSPACES].id;
-summonerName=json[SUMMONER_NAME_NOSPACES].name;
-    
-/* Retrieves the values in the data given by riot and sets it equal to the previously initialised variables   */    
-document.getElementById("name").innerHTML = summonerName;
-document.getElementById("sLevel").innerHTML = summonerLevel;
-document.getElementById("sID").innerHTML = summonerID;
+    /*If the summoner name is valid and thus retrieved, then this function executes */
+    if(SUMMONER_NAME !== ""){
 
-/* Calls the functions later declared in the script - List all the other functions to be called below! */    
-getMasteries(summonerID);
-getRunes(summonerID);
+    /* Use the dollar sign dot notation with ajax to start running an ajax script */    
+    $.ajax({
 
-},
+    /* This is the API request algorithm - asks the api for a particular function and returns the information */    
+    url:
+    'https://euw.api.pvp.net/api/lol/euw/v1.4/summoner/by-name/'+ SUMMONER_NAME + '?api_key=c53b69b4-e95f-491a-a79c-e8d8b34ab98f',
+    type: 'GET', /* Tells the script that this aims to 'retrieve' information  */
+    dataType:'json', /* Specify the data returned is JSON   */
+    data: {
 
-/* If Script is unsuccessful -> error is thrown  */    
-error:
-function(XMLHttpRequest,textStatus, errorThrown)
-{
-/* Send useful error message to user*/
-alert("error getting Summoner data! - Did you enter the name correctly?");
+    },
 
-}
+    /* If the script running is succcessful then this code block will execute */    
+    success:
+    function(json){
 
-});
-} else {}
-}
+    /* When the summoner name is entered without spaces -> inserts spaces into the string   */    
+    var SUMMONER_NAME_NOSPACES = SUMMONER_NAME.replace("","");
+    SUMMONER_NAME_NOSPACES = SUMMONER_NAME_NOSPACES.toLowerCase().trim();
 
-/* GetMasteries function below */
-function getMasteries(summonerID){
-$.ajax({
-  url:
-  'https://euw.api.pvp.net/api/lol/euw/v1.4/summoner/'+ summonerID + '/masteries?api_key=c53b69b4-e95f-491a-a79c-e8d8b34ab98f',
-  type: 'GET',
-  dataType: 'json',
-  data: {
+    /* Initialises All values to be used in the search function - var = json[summname].valinapi    */    
+    summonerLevel=json[SUMMONER_NAME_NOSPACES].summonerLevel;
+    summonerID=json[SUMMONER_NAME_NOSPACES].id;
+    summonerName=json[SUMMONER_NAME_NOSPACES].name;
 
-  },
-    /*function(resp) as it isn't the primary function in the script */
-  success: function(resp){
-    numberOfPages = resp[summonerID].pages.length;
+    /* Retrieves the values in the data given by riot and sets it equal to the previously initialised variables   */    
+    document.getElementById("name").innerHTML = summonerName;
+    document.getElementById("sLevel").innerHTML = summonerLevel;
+    document.getElementById("sID").innerHTML = summonerID;
 
-    document.getElementById("masteryPageCount").innerHTML=
-    numberOfPages;
-    resp[summonerID].pages.forEach(function (item){
-      document.getElementById("masteryPagesAll").innerHTML =
-      document.getElementById("masteryPagesAll").innerHTML +
-      item.name + "<br />";
+    /* Calls the functions later declared in the script - List all the other functions to be called below! */    
+    getMasteries(summonerID);
+    getRunes(summonerID);
+
+    },
+
+    /* If Script is unsuccessful -> error is thrown  */    
+    error:
+    function(XMLHttpRequest,textStatus, errorThrown)
+    {
+    /* Send useful error message to user*/
+    alert("error getting Summoner data! - Did you enter the name correctly?");
+
+    }
+
+    });
+    } else {}
+    }
+
+    /* GetMasteries function below */
+    function getMasteries(summonerID){
+    $.ajax({
+      url:
+      'https://euw.api.pvp.net/api/lol/euw/v1.4/summoner/'+ summonerID + '/masteries?api_key=c53b69b4-e95f-491a-a79c-e8d8b34ab98f',
+      type: 'GET',
+      dataType: 'json',
+      data: {
+
+      },
+        /*function(resp) as it isn't the primary function in the script */
+      success: function(resp){
+        numberOfPages = resp[summonerID].pages.length;
+
+        document.getElementById("masteryPageCount").innerHTML=
+        numberOfPages;
+        resp[summonerID].pages.forEach(function (item){
+          document.getElementById("masteryPagesAll").innerHTML =
+          document.getElementById("masteryPagesAll").innerHTML +
+          item.name + "<br />";
+
+        });
+
+      },
+      error: function(XMLHttpRequest, textStatus, errorThrown){
+      alert("error getting summoner data 2!");
+      }
+
+
 
     });
 
-  },
-  error: function(XMLHttpRequest, textStatus, errorThrown){
-  alert("error getting summoner data 2!");
-  }
+    }
 
-
-
-});
-
-}
-
-function getRunes(summonerID){
-  $.ajax({
-    url: 'https://euw.api.pvp.net/api/lol/euw/v1.4/summoner/'+ summonerID + '/runes?api_key=c53b69b4-e95f-491a-a79c-e8d8b34ab98f',
-    type: 'GET',
-    dataType: 'json',
-    data: {
+    function getRunes(summonerID){
+      $.ajax({
+        url: 'https://euw.api.pvp.net/api/lol/euw/v1.4/summoner/'+ summonerID + '/runes?api_key=c53b69b4-e95f-491a-a79c-e8d8b34ab98f',
+        type: 'GET',
+        dataType: 'json',
+        data: {
+        },
+        success: function(resp){
+          numberOfRPages = resp[summonerID].pages.length;
+          document.getElementById("runePageCount").innerHTML=numberOfRPages;
+          resp[summonerID].pages.forEach(function(item){
+            document.getElementById("runePagesAll").innerHTML =
+            document.getElementById("runePagesAll").innerHTML +
+            item.name + "<br />";
+      });
     },
-    success: function(resp){
-      numberOfRPages = resp[summonerID].pages.length;
-      document.getElementById("runePageCount").innerHTML=numberOfRPages;
-      resp[summonerID].pages.forEach(function(item){
-        document.getElementById("runePagesAll").innerHTML =
-        document.getElementById("runePagesAll").innerHTML +
-        item.name + "<br />";
-  });
-},
-error: function(XMLHttpRequest, textStatus, errorThrown){
-alert("error getting summoner data 3!");
-}
+    error: function(XMLHttpRequest, textStatus, errorThrown){
+    alert("error getting summoner data 3!");
+    }
+    });
+    }
 });
-}
